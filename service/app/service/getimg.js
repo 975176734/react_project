cc02c28df4d1714cc57f8b532d064ced78790246
Febrie-111 <3477349668@qq.com>
2020-05-28 19:53:21 +0800

    1

--- service/app/service/getimg.js
+++ service/app/service/getimg.js
@@ -2,7 +2,9 @@ const Service = require('egg').Service;
 class getimgUserService extends Service {
   async getimg(obj) {
     const sql=`select * from picture where species="${obj.species}"`
+    console.log(sql)
     const data = await this.app.mysql.query(sql);
+    console.log(data)
     return data;
   }
 }
